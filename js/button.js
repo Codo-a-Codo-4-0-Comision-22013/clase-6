8617699d8d3dc32d14c250212e769aa322ab3228
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2022-03-28 19:38:27 -0300

    prototype functions

--- js/button.js
+++ js/button.js
@@ -1,9 +1,14 @@
+import render, { render2 } from './render_functions.js';
+
 export default class Button {
     constructor(text) {
         this.text = text;
     }
 
-    render() {
+    /*render() {
         return `<button>${this.text}</button>`;
-    }
+    }*/
 }
+
+
+Button.prototype.render = render2;
---
+++ js/render_functions.js
@@ -0,0 +1,7 @@
+export default function render() {
+    return `<button>${this.text}</button>`;
+}
+
+export function render2() {
+    return `<h2><button>${this.text}</button><h2>`;
+}
